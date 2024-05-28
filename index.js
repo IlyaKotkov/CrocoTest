9d3bdc1ce118aafdfd40fc73cafcfe0744d5ac4e
Ilya Kotkov <MopsBox2552@yandex.ru>
2024-05-28 21:20:36 +0300

    доделана шапка крошки и начат главный блок

---
+++ index.js
@@ -0,0 +1,18 @@
+function formatDate(date) {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear().toString().substr(-2);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+  
+    return `${day} ${month} ${year} | ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+  }
+
+  const datetime = document.getElementById('datetime');
+  const currentDate = new Date();
+
+  const yesterday = new Date(currentDate);
+  yesterday.setDate(currentDate.getDate() - 1);
+
+  datetime.innerText = formatDate(yesterday);
