44e129d64c17b4ce4fe58785add03c885a564329
Ilya Kotkov <MopsBox2552@yandex.ru>
2024-05-30 14:16:10 +0300

    доделан сайт

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CrocoTest
+
+**Интро**
+
+Верстка копии сайта для тестового задания
+
+⚙️ Стек
+* HTML5, CSS3;
+* JavaScript ES6+;
+* flexBox;
+* БЭМ;
+* анимации;
+* медиа-запросы;
+* Grid Layot;
+
+[ссылка на сайт](https://www.idntimes.com/korea/kpop/zahra-fadillah-haerunisa/pro-kontra-mv-newjeans-cool-with-you-c1c2)
+
+[ссылка на github pages]()
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function formatDate(date) {
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = date.getFullYear().toString().substr(-2);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-  
-    return `${day} ${month} ${year} | ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-  }
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear().toString().substr(-2);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
 
-  const datetime = document.getElementById('datetime');
-  const currentDate = new Date();
+  return `${day} ${month} ${year} | ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+}
 
-  const yesterday = new Date(currentDate);
-  yesterday.setDate(currentDate.getDate() - 1);
+const datetime = document.getElementById('datetime');
+const currentDate = new Date();
 
-  datetime.innerText = formatDate(yesterday);
+const yesterday = new Date(currentDate);
+yesterday.setDate(currentDate.getDate() - 1);
+
+datetime.innerText = formatDate(yesterday);
